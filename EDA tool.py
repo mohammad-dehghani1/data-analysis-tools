889c784b96c9c41903744694a1150a63da873d47
Mohammad Dehghani <mr.md878@gmail.com>
2024-02-19 08:59:26 +0330

    Update EDA tool.py

--- EDA tool.py
+++ EDA tool.py
@@ -2,15 +2,9 @@
 """
 Created on Wed Feb  2 17:30:10 2022
 
-@author: Rayanet
+@author: Mohammad
 """
 
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> 9af56298869de8a3b999af2e8edcfcf2cc7988e4
 import pandas as pd
 import seaborn as sns
 import sweetviz as sv
@@ -31,7 +25,3 @@ def analyze(report_kind):
     if report_kind == 'whole_analyze':
         my_report = sv.analyze(df, target_feat= 'amnt')
         my_report.show_html()
-<<<<<<< HEAD
-
-=======
->>>>>>> 9af56298869de8a3b999af2e8edcfcf2cc7988e4
